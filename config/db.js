4c948f7b3e0edcb9b9251d11cafda80069a20bf9
EgiPegi <pegi.rosidi@gmail.com>
2021-01-27 11:01:32 +0700

    edit db commit

--- config/db.js
+++ config/db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-    user: 'root',
-    password: '',
-    host: 'localhost',
-    database: 'divergent', 
+    user: 'id16018198_dinyok',
+    password: 'tRSObcw@H9l{&<xC',
+    host: 'databases.000webhost.com',
+    database: 'id16018198_divergent', 
 })
 
 module.exports=db;
+//tRSObcw@H9l{&<xC
